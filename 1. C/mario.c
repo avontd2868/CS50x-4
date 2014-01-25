d29efd4353e0e539641f072f7d309cdc69c13534
skandebaba <johann.padkos@gmail.com>
2014-01-25 19:43:30 +1300

    Added Comments
    Added comments to make it more clear what each part of the code does

--- 1. C/mario.c
+++ 1. C/mario.c
@@ -8,39 +8,52 @@
  *  Written by Johann Nel
  */
  
- #include <cs50.h>
- #include <stdio.h>
+#include <cs50.h>
+#include <stdio.h>
  
- int main(void)
- {
+int main(void)
+{
     int height = 0;
     int blocks = 1;
-    
+   
+    /*
+     *  Prompt the user to specify the height of the half-pyramid
+     *  If the height is less than 0 or more than 23 it will print
+     *  a message i.e. "Height: -1" and re-prompt the user to specify
+     *  the height of the half-pyramid. Will only break out of the forever
+     *  loop if the height is between 0 and 23
+     */
     for (;;)
     {
         printf("Please enter the height of the half-pyramid: ");
         height = GetInt();
-        
+       
         if (height >= 0 && height <= 23)
             break;
-        
+       
         printf("Height: %d\n", height);
     }
-    
+   
+    /*
+     *  Print the half pyramid of height the user specified earlier
+     *  First print the approriate amount of spaces
+     *  Then print the approriate amount of blocks ('#')
+     *  Finally print the new line character and move onto the next row
+     *  Will continue doing this until specified height is reached
+     */    
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < height - blocks; j++)
         {
             printf(" ");
         }
-        
+       
         for (int k = 0; k < blocks + 1; k++)
         {
             printf("#");
         }
-        
+       
         blocks++;
         printf("\n");
-    }
-  
+   }  
  }
