f41e4234809d7823b330d2c8edbfe14a4fd13531
skandebaba <johann.padkos@gmail.com>
2014-02-19 09:23:12 +1300

    Added Searching Algorithm
    Made use of Linear Search Algorithm

--- 3. Game of Fifteen/find/helpers.c
+++ 3. Game of Fifteen/find/helpers.c
@@ -8,15 +8,25 @@
  */
        
 #include <cs50.h>
+#include <stdio.h>
 
 #include "helpers.h"
 
-/**
+/*
  * Returns true if value is in array of n values, else false.
  */
 bool search(int value, int values[], int n)
 {
-    // TODO: implement a searching algorithm
+    // Look at each element in the "haystack"
+    // If values[i] is equal to the value we are looking for
+    // return true
+    for (int i = 0; i < n; i++)
+    {
+        if (values[i] == value)
+            return true;
+    }
+    
+    // Return false
     return false;
 }
 
@@ -25,6 +35,6 @@ bool search(int value, int values[], int n)
  */
 void sort(int values[], int n)
 {
-    // TODO: implement an O(n^2) sorting algorithm
-    return;
+	// TODO: implement an O(n^2) sorting algorithm
+    return;  
 }
