062f54d5bd029e53e4bdddc8462ee478f81625fa
skandebaba <johann.padkos@gmail.com>
2014-02-19 09:17:26 +1300

    Added Comments
    Added comments as per specifications

--- 3. Game of Fifteen/find/generate.c
+++ 3. Game of Fifteen/find/generate.c
@@ -21,17 +21,21 @@
 
 int main(int argc, string argv[])
 {
-    // TODO: comment me
+    // Print an error message to the user 
+    // if no command-line arguments specified
     if (argc != 2 && argc != 3)
     {
         printf("Usage: ./generate n [s]\n");
         return 1;
     }
 
-    // TODO: comment me
+    // Convert the second command-line argument to an integer
     int n = atoi(argv[1]);
 
-    // TODO: comment me
+    // If there are three command-line arguments
+    // seed rand() with the value specified by 3rd command-line argument
+    // and cast to an unsigned integer
+    // else use the default time to seed rand()
     if (argc == 3)
     {
         srand((unsigned int) atoi(argv[2]));
@@ -41,7 +45,8 @@ int main(int argc, string argv[])
         srand((unsigned int) time(NULL));
     }
 
-    // TODO: comment me
+    // Print specified number of random numbers specified by user
+    // And limit the upperbound
     for (int i = 0; i < n; i++)
     {
         printf("%i\n", rand() % LIMIT);
